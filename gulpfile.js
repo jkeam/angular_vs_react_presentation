5287d33f9dc4ea59916bb4a94d80ccee6a3c7e61
Jon Keam <jpkeam@gmail.com>
2015-03-02 02:10:23 -0500

    first draft of presentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var browserify = require('gulp-browserify');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('templates', function() {
   var locals = {};
@@ -26,8 +27,13 @@ gulp.task('copy', function(){
   gulp.src('./src/*.css').pipe(gulp.dest('./dist/public'));
   gulp.src('./src/*.png').pipe(gulp.dest('./dist/public'));
   gulp.src(['./src/league-gothic/**/*']).pipe(gulp.dest('./dist/public/league-gothic'));
+  gulp.src(['./src/source-sans-pro/**/*']).pipe(gulp.dest('./dist/public/source-sans-pro'));
 });
 
+gulp.task('dev', function() {
+  gulp.watch('./src/*', ['default']);
+  nodemon({ script: 'app.js' });
+});
 
 gulp.task('default', ['templates', 'scripts', 'copy']);
 
